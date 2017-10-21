59f436df2f9994f069e411a15e4643e1d0c192e3
Batlijo <batlijoselevitz@gmail.com>
2017-10-21 09:13:49 -0700

    beginning of day 3 class notes

--- mynotes.js
+++ mynotes.js
@@ -1,8 +1,18 @@
 /* GIPHY API
 
-WHAT DOES GIPHY API MEAN??
+WHAT DOES  API MEAN??
+In computer programming, an application programming interface (API)
+is a set of subroutine definitions, protocols, and tools for building 
+application software. ... A good API makes it easier to develop a computer 
+program by providing all the building blocks, which are then put together 
+by the programmer. PRE-DEFINED ROUTINES, you have to work within the framework.
+MOST API'S ARE NOT OPEN SOURCE. API's are an absraction layer, the notion you don't 
+need to understand everything right away.It's abstracting away complexity so you can
+focus on more important things.
+USE CASES:
+weather channel, performing action, 
 
-AJAX REQUEST SYNTAX */
+AJAX REQUEST SYNTAX, its asynchronous and because of this you can't determine how long it will take to a load very difficult to debug  */
 $.ajax({url:queryURL,method:'GET'})
 .DONE(function(response)(
 	console.log(response);
@@ -12,7 +22,7 @@ $.ajax({url:queryURL,method:'GET'})
   .DONE(function(response)( this function happens once info has been retrieved, 
   this function returns a response and console log it. when you see it in a console 
   you'll see objects collected and an array of data. We have collected data from an api and 
-  brought in into an html application.
+  brought it in into an html application.
 
 BUTTON TRIGGERED AJAX
 - build html document
@@ -27,6 +37,10 @@ why not add an id tag? what does data-search tag do????
 	$("button").on("click",function(){
 		console.log("BUTTON CLICKED!")
 	})
-// </script> the console log is to confirm that when button is clicked it was clicked aka that it works woooo
-
-
+/* </script> the console log is to confirm that when button is clicked it was clicked aka that it works woooo
+JSON aka Javascript Object Notation: a way to represent data, it is a file format
+WHY DO WE USE JSON? Makes it readable to send data between servers back and forth
+Disadvantages of JSON:
+WHAT JQUERY METHOD DO WE USE TO RETRIEVE DATA FROM A URL DATABASE? AJAX
+WHAT TWO PARAMETERS DO WE PASS INTO AJAX TO RETRIVE DATA ONLINE? URL AND GET METHOD
+ 
